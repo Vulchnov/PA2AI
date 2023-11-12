d9d39666ff3ecb52a6b5bbebddca25dfc07b2caf
Esmith7405 <100811484+Esmith7405@users.noreply.github.com>
2023-11-11 19:05:24 -0700

    Boigis

--- main.py
+++ main.py
@@ -44,56 +44,56 @@ def URMove(gameBoard): #Uniform Random
 
 def DLMMMove(gameBoard, player, ply): #Depth-Limited MinMax - Returns a move for a given player - NEED TO MAKE IT UNIVERSAL (Have DLMM Assume the player is always yellow? invert board every turn?)
     valid_moves = gameBoard.getValidMoves()
-    print(valid_moves)
+    #print(valid_moves)
     bestMove = -1
     #Return action in random.choice(valid_moves) that is highest for MinValue(a, state, ply)
     root = gameTree.Node(None, gameBoard) #instantiate root node
     if (player == 'Y'):
         maxUtil = (math.inf * -1)
         for move in valid_moves:
-            print(f"Placing piece in row: {move}")
-            minVal = MinValue(gameTree.Node(root, root.board.exploreMove(player, move)), player, ply-1)
+            #print(f"Placing piece in row: {move}")
+            minVal = MinValue(gameTree.Node(root, root.board.exploreMove(player, move)), ply-1)
             if (minVal > maxUtil):
                 bestMove = move
     elif(player == 'R'):
         maxUtil = (math.inf)
         for move in valid_moves:
-            print(f"Placing piece in row: {move}")
-            minVal = MaxValue(gameTree.Node(root, root.board.exploreMove(player, move)), player, ply-1)
+            #print(f"Placing piece in row: {move}")
+            minVal = MaxValue(gameTree.Node(root, root.board.exploreMove(player, move)), ply-1)
             if (minVal < maxUtil):
                 bestMove = move
-    print(f"Move Selected, Column: {bestMove}")
+    #print(f"Move Selected, Column: {bestMove}")
     return bestMove
 
-def MaxValue(state, player, ply): #Returns a Utility Value
+def MaxValue(state, ply): #Returns a Utility Value
     if(state.isTerminal() or ply <= 0):
-        print(f"Terminal State Reached, ply = {ply}, util = {state.getUtil()}, Board:\n{state.board}")
+        #print(f"Terminal State Reached, ply = {ply}, util = {state.getUtil()}, Board:\n{state.board}")
         return state.getUtil()
     v = math.inf * -1
     valid_moves = gameBoard.getValidMoves()
     for move in valid_moves:
-        print(f"Exploring move: {move}")
-        v = max(v,MinValue(gameTree.Node(state, state.board.exploreMove('Y', move)), 'Y', ply-1)) #Replace player with 'Y'?
+        #print(f"Exploring move: {move}")
+        v = max(v,MinValue(gameTree.Node(state, state.board.exploreMove('Y', move)), ply-1)) #Replace player with 'Y'?
     return v
 
-def MinValue(state, player, ply): #Returns a Utility Value
+def MinValue(state, ply): #Returns a Utility Value
     if(state.isTerminal() or ply <= 0):
-        print(f"Terminal State Reached, ply = {ply}, util = {state.getUtil()}, Board:\n{state.board}")
+        #print(f"Terminal State Reached, ply = {ply}, util = {state.getUtil()}, Board:\n{state.board}")
         return state.getUtil()
     v = math.inf
     valid_moves = gameBoard.getValidMoves()
     for move in valid_moves:
-        print(f"Exploring move: {move}")
-        v = min(v,MaxValue(gameTree.Node(state, state.board.exploreMove('R', move)), 'R', ply-1)) #Replace player with 'R'?
+        #print(f"Exploring move: {move}")
+        v = min(v,MaxValue(gameTree.Node(state, state.board.exploreMove('R', move)), ply-1)) #Replace player with 'R'?
     return v
 
-def PMCGSMove(gameBoard): #Pure Monte Carlo Game Search
+def PMCGSMove(gameBoard, player): #Pure Monte Carlo Game Search
     mct = mcts.MCTS(gameBoard, player, 1)
     mct.search(5)
     move = mct.best_move()
     return move
 
-def UCTMove(gameBoard): #Upper Confidence bound for Trees
+def UCTMove(gameBoard, player): #Upper Confidence bound for Trees
     uctT = UCT.UCT(gameBoard, player, 1)
     uctT.search(5)
     move = uctT.best_move()
@@ -105,8 +105,27 @@ moveChosen = URMove(gameBoard)
 print(moveChosen)
 print(gameBoard.pieceTracker)
 '''
-gameBoard.insertPiece(player, DLMMMove(gameBoard, player, 2))
-print(gameBoard)
+#gameBoard.insertPiece(player, DLMMMove(gameBoard, player, 5))
+#print(gameBoard)
 
-#print(PMCGSMove(gameBoard))
 #Tournament
+gameBoard = board.Board([['O','O','O','O','O','O','O'],
+                         ['O','O','O','O','O','O','O'],
+                         ['O','O','O','O','O','O','O'],
+                         ['O','O','O','O','O','O','O'],
+                         ['O','O','O','O','O','O','O'],
+                         ['O','O','O','O','O','O','O']])
+currPlayer = 'Y'
+while(gameBoard.gameOver() == False):
+    match currPlayer:
+        case 'Y':
+            gameBoard.insertPiece('Y', DLMMMove(gameBoard, 'Y', 5))
+            print(gameBoard)
+            currPlayer = 'R'
+        case 'R':
+            gameBoard.insertPiece('R', DLMMMove(gameBoard, 'R', 5))
+            print(gameBoard)
+            currPlayer = 'Y'
+        case _:
+            print("Error, shits fucked, yo")
+print(f"Game End: \n{gameBoard}\nWinner: {gameBoard.getWinner()}")
